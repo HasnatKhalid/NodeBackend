215349afd7cea933c184e113e4bef4f9b9074842
Hasnat Khalid <hasnatkhalid333@gmail.com>
2024-02-16 16:26:01 +0500

    Github data added

--- index.js
+++ index.js
@@ -3,6 +3,44 @@ require("dotenv").config();
 const app = express();
 const port = 4000;
 
+const githubData = {
+  login: "HasnatKhalid",
+  id: 95873135,
+  node_id: "U_kgDOBbbobw",
+  avatar_url: "https://avatars.githubusercontent.com/u/95873135?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/HasnatKhalid",
+  html_url: "https://github.com/HasnatKhalid",
+  followers_url: "https://api.github.com/users/HasnatKhalid/followers",
+  following_url:
+    "https://api.github.com/users/HasnatKhalid/following{/other_user}",
+  gists_url: "https://api.github.com/users/HasnatKhalid/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/HasnatKhalid/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/HasnatKhalid/subscriptions",
+  organizations_url: "https://api.github.com/users/HasnatKhalid/orgs",
+  repos_url: "https://api.github.com/users/HasnatKhalid/repos",
+  events_url: "https://api.github.com/users/HasnatKhalid/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/HasnatKhalid/received_events",
+  type: "User",
+  site_admin: false,
+  name: null,
+  company: null,
+  blog: "",
+  location: null,
+  email: null,
+  hireable: null,
+  bio: null,
+  twitter_username: null,
+  public_repos: 8,
+  public_gists: 0,
+  followers: 0,
+  following: 0,
+  created_at: "2021-12-09T16:18:58Z",
+  updated_at: "2024-02-13T17:47:06Z",
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -17,6 +55,10 @@ app.get("/login", (req, res) => {
 
 app.get("/youtube", (rep, res) => {
   res.send("<h2>Chai aur Code</h2>");
+});
+
+app.get("/github", (rep, res) => {
+  res.json(githubData);
 });
 
 app.listen(process.env.PORT, () => {
